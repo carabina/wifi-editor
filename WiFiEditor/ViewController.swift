e1f35b1a52cc25b1aa5fae2b1f79caff57d54858
Simson Garfinkel <simsong@acm.org>
2015-09-20 21:12:32 -0400

    Stores wi-fi stuff as /etc/xxx-3

--- WiFiEditor/ViewController.swift
+++ WiFiEditor/ViewController.swift
@@ -3,22 +3,34 @@
 //
 //  Copyright (c) 2015 Vea Software. All rights reserved.
 //
+// http://stackoverflow.com/questions/13553935/nstableview-to-allow-user-to-choose-which-columns-to-display
+// http://stackoverflow.com/questions/19801271/how-can-i-make-command-a-select-all-the-nstextview-text-in-rows-in-an-nstablevie
+
+// https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man1/authopen.1.html#//apple_ref/doc/man/1/authopen
 
 import Cocoa
+import Foundation
 
 class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate
 {
 
     @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var totalNetworksCell: NSTextField!
+    @IBOutlet weak var totalSelectedCell: NSTextField!
+    @IBOutlet weak var searchField: NSSearchField!
+    @IBOutlet weak var statusField: NSTextField!
+    
+
     var networks = Dictionary<String, Dictionary<String, AnyObject>>()
-    var objects  = NSMutableArray()
-    var selected =  Set<String>()
+    var displayedNetworks  = NSMutableArray()
+    var selectedNetworks =  Set<String>()
+    var airport_preferences = Dictionary<String, AnyObject>()
+    let airport_preferences_fname = "/Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var ignoreSet = Set<String>()
-        
         if let path = NSBundle.mainBundle().pathForResource("config", ofType: "plist") {
             if let config = NSDictionary(contentsOfFile: path) as? Dictionary<String, AnyObject> {
                 let ignoreArray = config["ignore"] as! Array<String>
@@ -26,13 +38,14 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
             }
         }
         var networkProperties = Set<String>()
-        let path = "/Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist"
-        if let dict = NSDictionary(contentsOfFile: path) as? Dictionary<String, AnyObject> {
+        if let dict = NSDictionary(contentsOfFile: airport_preferences_fname) as? Dictionary<String, AnyObject> {
+            airport_preferences = dict
             networks = dict["KnownNetworks"] as! Dictionary<String, Dictionary<String, AnyObject>>
-            // Go through all the networks, getting the
-            for (netName,networkVals) in networks {
-                objects.addObject(netName)         // so we can have a sorted list
-                for (propertyName, propertyVal) in (networkVals) {
+            totalNetworksCell!.integerValue = networks.count
+
+            // Go through all the networks, getting the property names
+            for (_,networkVals) in networks {
+                for (propertyName, _) in (networkVals) {
                     if !ignoreSet.contains(propertyName){
                         networkProperties.insert(propertyName)
                     }
@@ -40,70 +53,107 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
             }
         }
         
-        
         // Now add all the property names as colums
-        for propertyName in networkProperties {
-            var column = NSTableColumn()
-            column.editable = false
-            column.minWidth = 100
-            column.headerCell = NSTableHeaderCell()
-            column.headerCell.setObjectValue(propertyName)
-            self.tableView.addTableColumn(column)
-        }
-        
-        self.tableView.rowSizeStyle = NSTableViewRowSizeStyle.Default
+        //for propertyName in networkProperties {
+        //    let column = NSTableColumn(identifier: propertyName)
+        //    column.width = 50
+        //    column.title = propertyName
+        //    column.editable = false
+        //    self.tableView.addTableColumn(column)
+        //}
         self.tableView.reloadData()
+        displayNetworksMatching("")
     }
 
-    override var representedObject: AnyObject? {
-            didSet {
-        // Update the view, if already loaded.
+    
+    func displayNetworksMatching(s:String) {
+        displayedNetworks.removeAllObjects()
+        for (netName,networkVals) in networks {
+            for (_,val) in networkVals {
+                if let val_string = val as? String {
+                    if s=="" || (val_string.lowercaseString.rangeOfString(s.lowercaseString) != nil) {
+                        displayedNetworks.addObject(netName)
+                        break
+                    }
+                }
+            }
         }
+        totalNetworksCell.integerValue = networks.count
+        self.tableView.reloadData()
+    }
+    
+    @IBAction func search(sender: NSSearchField) {
+        displayNetworksMatching(sender.stringValue)
     }
 
     func numberOfRowsInTableView(tableView: NSTableView) -> Int {
-        return self.objects.count
+        return self.displayedNetworks.count
     }
-    
-    func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject?
-    {
-        //var col = NSNumberFormatter().numberFromString(tableColumn!.title)
-        //println("col="+String(col!.integerValue))
-        
-        tableColumn!.width = 40
-        //var cellView = tableView.makeViewWithIdentifier("cell", owner: self) as! NSTableCellView
-        
-        
-        if let title = tableColumn?.title {
-            let networkName = objects.objectAtIndex(row) as! String
-            if title == "select" {
-                return selected.contains(networkName) || tableView.selectedRowIndexes.containsIndex(row)
-            }
-            let networkVals = networks[networkName]!
-            if let val: AnyObject = networkVals[title]  {
-                return "\(val)"
-            }
-        }
-        return ""
 
-        //cellView.textField!.stringValue = "\(pow(Double(row),col!.doubleValue))"
-        
-        //return cellView
+   
+    //if title == "select" {
+    //    return selectedNetworks.contains(networkName) || tableView.selectedRowIndexes.containsIndex(row)
+    //}
+ 
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let identifier = tableColumn!.identifier
+        let networkName = displayedNetworks.objectAtIndex(row) as! String
+        let networkVals = networks[networkName]!
+        if let val: AnyObject = networkVals[identifier]  {
+            var obj = tableView.makeViewWithIdentifier(identifier, owner:self)
+            if (obj==nil) {
+                // No existing cell to reuse, so make a new one
+                // https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/TableView/PopulatingView-TablesProgrammatically/PopulatingView-TablesProgrammatically.html#//apple_ref/doc/uid/10000026i-CH14-SW1
+                obj = NSTableCellView(frame:NSMakeRect(0,0,100,100))
+                obj!.identifier = identifier
+            }
+            let cellView = obj as! NSTableCellView
+            cellView.textField!.stringValue = "\(val)"
+            return cellView
+        }
+        return nil
     }
     
     func tableViewSelectionDidChange(notification: NSNotification)
     {
         if (self.tableView.numberOfSelectedRows > 0) {
-            if let networkName = self.objects.objectAtIndex(self.tableView.selectedRow) as? String {
-                if selected.contains(networkName){
-                    selected.remove(networkName)
+            if let networkName = self.displayedNetworks.objectAtIndex(self.tableView.selectedRow) as? String {
+                if selectedNetworks.contains(networkName){
+                    selectedNetworks.remove(networkName)
                 } else {
-                    selected.insert(networkName)
+                    selectedNetworks.insert(networkName)
                 }
             }
-            self.tableView.deselectRow(self.tableView.selectedRow)
+            //self.tableView.deselectRow(self.tableView.selectedRow)
         }
         
     }
+    @IBAction func save(sender: NSControl) {
+        let d = airport_preferences as NSDictionary
+        let fname = NSTemporaryDirectory() + "/preferences.new"
+        d.writeToFile(fname,atomically:false)
+        print("written to",fname)
+        
+        let old_signal = signal(SIGPIPE,SIG_IGN)
+        let data =  NSData(contentsOfFile: fname)
+        let task = NSTask()
+        task.launchPath = "/usr/libexec/authopen"
+        task.arguments = ["-c","-w","/etc/xxx-3"]
+        let pipe = NSPipe()
+        task.standardInput = pipe
+        task.launch()
+        pipe.fileHandleForWriting.writeData(data!)
+        pipe.fileHandleForWriting.closeFile()
+        task.waitUntilExit()
+        signal(SIGPIPE,old_signal)
+    }
+    
+    @IBAction func deleteSelected(sender: NSButton) {
+        for i in self.tableView.selectedRowIndexes {
+            print("row",i,"is selected",displayedNetworks[i],networks[displayedNetworks[i] as! String])
+        }
+        
+    }
+    
 }
 
